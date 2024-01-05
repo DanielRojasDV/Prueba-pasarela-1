bcb66379312e9c9b1248a7f08082d66ad516a0e2
Daniel Rojas <danielr.dv9@gmail.com>
2024-01-05 09:24:09 -0500

    Primer Commit. Seguimiento unicamente de los elementos del tema hijo.

---
+++ wp-content/themes/astra-child/js/funciones.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+    var botonSeleccionarTodo = document.getElementById('selectAllButton');
+
+    botonSeleccionarTodo.addEventListener('click', function(){
+        alert('El script funciona!');
+    })
+}
